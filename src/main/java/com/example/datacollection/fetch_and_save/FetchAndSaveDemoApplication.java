cfeecbeb14892d9371e26f26396a3591f1fef400
TyTe108 <Tylim@ucsc.edu>
2024-05-24 04:07:36 -0700

    first commit

---
+++ src/main/java/com/example/datacollection/fetch_and_save/FetchAndSaveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacollection.fetch_and_save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchAndSaveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchAndSaveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datacollection/fetch_and_save/FetchAndSaveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacollection.fetch_and_save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchAndSaveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
