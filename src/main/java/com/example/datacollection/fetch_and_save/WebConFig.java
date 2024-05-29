de183c10de1a0ecc7b539c1038798de15fc444a7
TyTe108 <Tylim@ucsc.edu>
2024-05-28 17:55:58 -0700

    Enabled CORS

---
+++ src/main/java/com/example/datacollection/fetch_and_save/WebConFig.java
@@ -0,0 +1,19 @@
+package com.example.datacollection.fetch_and_save;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://fetch-and-save-d8b0e2b25bdc.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
