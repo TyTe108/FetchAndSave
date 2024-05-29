857d6287fcd75110922016769e14fa954c7eda52
TyTe108 <Tylim@ucsc.edu>
2024-05-28 18:23:08 -0700

    Changed webconfig a bit

--- src/main/java/com/example/datacollection/fetch_and_save/WebConfig.java
+++ src/main/java/com/example/datacollection/fetch_and_save/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.datacollection.fetch_and_save;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://fetch-and-save-d8b0e2b25bdc.herokuapp.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
